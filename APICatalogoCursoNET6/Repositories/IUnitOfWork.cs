1761257f2a1f3449cdc1a45b5d30cf276c4ebabf
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2024-01-13 11:32:14 -0300

    Início da criação do padrão UnitOfWork

---
+++ APICatalogoCursoNET6/Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogoCursoNET6.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IProdutoRepository ProdutoRepository { get; }
+        ICategoriaRepository CategoriaRepository { get; }
+        void Commit();
+    }
+}
---
+++ APICatalogoCursoNET6/Repositories/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using APICatalogoCursoNET6.Data;
+
+namespace APICatalogoCursoNET6.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ProdutoRepository _produtoRepository;
+        private CategoriaRepository _categoriaRepository;
+        public AppDbContext _appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IProdutoRepository ProdutoRepository
+        {
+            get
+            {
+                return _produtoRepository = _produtoRepository ?? new ProdutoRepository(_appDbContext);
+            }
+        }
+
+        public ICategoriaRepository CategoriaRepository
+        {
+            get
+            {
+                return _categoriaRepository = _categoriaRepository ?? new CategoriaRepository(_appDbContext);
+            }
+        }
+
+        public void Commit()
+        {
+            _appDbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _appDbContext.Dispose();
+        }
+    }
+}
