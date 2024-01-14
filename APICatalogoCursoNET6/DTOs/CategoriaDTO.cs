414d816a93c21b623492d24f12ad986ac91b7920
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2024-01-14 16:54:51 -0300

    criação dos DTOs

---
+++ APICatalogoCursoNET6/DTOs/CategoriaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogoCursoNET6.DTOs
+{
+    public class CategoriaDTO
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+        public ICollection<ProdutoDTO>? Produtos { get; set; }
+    }
+}
