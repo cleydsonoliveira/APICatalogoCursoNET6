b0bf1fa834063e10484732f6a61e7c1fbc020b1f
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2024-01-20 17:19:46 -0300

    Revert "adição no repository"
    This reverts commit 852cc87d2738010ccd2c2f134260d449d8fd0cc9.

--- APICatalogoCursoNET6/Repositories/IProdutoRepository.cs
+++ APICatalogoCursoNET6/Repositories/IProdutoRepository.cs
@@ -1,11 +1,9 @@
 ﻿using APICatalogoCursoNET6.Models;
-using APICatalogoCursoNET6.Pagination;
 
 namespace APICatalogoCursoNET6.Repositories
 {
     public interface IProdutoRepository : IRepository<Produto>
     {
-        IEnumerable<Produto> GetProdutos(ProdutosParameters parameters);
         IEnumerable<Produto> GetProdutosPorPreco();
     }
 }
