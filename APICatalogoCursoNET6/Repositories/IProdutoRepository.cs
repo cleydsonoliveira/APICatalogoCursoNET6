852cc87d2738010ccd2c2f134260d449d8fd0cc9
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2024-01-20 17:17:40 -0300

    adição no repository

--- APICatalogoCursoNET6/Repositories/IProdutoRepository.cs
+++ APICatalogoCursoNET6/Repositories/IProdutoRepository.cs
@@ -1,9 +1,11 @@
 ﻿using APICatalogoCursoNET6.Models;
+using APICatalogoCursoNET6.Pagination;
 
 namespace APICatalogoCursoNET6.Repositories
 {
     public interface IProdutoRepository : IRepository<Produto>
     {
+        IEnumerable<Produto> GetProdutos(ProdutosParameters parameters);
         IEnumerable<Produto> GetProdutosPorPreco();
     }
 }
