f3bbd95cd8084faa400d135965db825474144ef4
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2024-01-20 18:13:28 -0300

    ajustes no repositório de categorias

--- APICatalogoCursoNET6/Repositories/CategoriaRepository.cs
+++ APICatalogoCursoNET6/Repositories/CategoriaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using APICatalogoCursoNET6.Data;
 using APICatalogoCursoNET6.Models;
+using APICatalogoCursoNET6.Pagination;
 using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogoCursoNET6.Repositories
@@ -7,6 +8,16 @@ namespace APICatalogoCursoNET6.Repositories
     public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
     {
         public CategoriaRepository(AppDbContext appDbContext) : base(appDbContext) { }
+
+        public IEnumerable<Categoria> GetCategorias(CategoriasParameters categoriasParameters)
+        {
+            return GetCategorias(categoriasParameters)
+                .OrderBy(x => x.Nome)
+                .Skip((categoriasParameters.PageNumber - 1) * categoriasParameters.PageSize)
+                .Take(categoriasParameters.PageSize)
+                .ToList();
+        }
+
         public IEnumerable<Categoria> GetCategoriasProdutos()
         {
             return Get().Include(x => x.Produtos);
--- APICatalogoCursoNET6/Repositories/ICategoriaRepository.cs
+++ APICatalogoCursoNET6/Repositories/ICategoriaRepository.cs
@@ -1,9 +1,11 @@
 ﻿using APICatalogoCursoNET6.Models;
+using APICatalogoCursoNET6.Pagination;
 
 namespace APICatalogoCursoNET6.Repositories
 {
     public interface ICategoriaRepository : IRepository<Categoria>
     {
+        IEnumerable<Categoria> GetCategorias(CategoriasParameters categoriasParameters);
         IEnumerable<Categoria> GetCategoriasProdutos();
     }
 }
