f340cde363fb3f8bb23670f903929a57d0434d05
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2024-02-12 15:12:20 -0300

    transformação dos parametros em outra classe para reuso do código

--- APICatalogoCursoNET6/Pagination/ProdutosParameters.cs
+++ APICatalogoCursoNET6/Pagination/ProdutosParameters.cs
@@ -1,20 +1,4 @@
 ﻿namespace APICatalogoCursoNET6.Pagination
 {
-    public class ProdutosParameters
-    {
-        const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        public int _pageSize = 10;
-        public int PageSize
-        {
-            get
-            {
-                return _pageSize;
-            }
-            set
-            {
-                _pageSize = (value > maxPageSize) ? maxPageSize : value;
-            }
-        }
-    }
+    public class ProdutosParameters : QueryStringParameters { }
 }
